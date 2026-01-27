7ac822bcfe9703f337a70e78a032ce04c122190b
RivieraImmobilien <binary20355@gmail.com>
2026-01-27 23:25:59 +0100

    Add NEST website files

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener('DOMContentLoaded', function(){
+  let slideIndex = 0;
+  showSlides();
+  function showSlides() {
+    let slides = document.querySelectorAll('.slider img');
+    if (slides.length == 0) return;
+    slides.forEach((slide) => { slide.style.display = 'none'; });
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex-1].style.display = 'block';
+    setTimeout(showSlides, 3000);
+  }
+});
